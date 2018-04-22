f99e2809774d9df327476204a73bccfa86cc40fa
FlyingWithJerome <jiarunm@uci.edu>
2018-04-21 21:00:26 -0400

    add message text box

--- test/GUI_interface.py
+++ test/GUI_interface.py
@@ -16,42 +16,45 @@ from face_detection import Detector
 
 class Interface(object):
 
-    def __init__(self):
+    def __init__(self, camera_num=0):
         self.__gui_root = tk.Tk()
         self.__btn = tk.Button(self.__gui_root, text="Exit!", command=self.__destroy)
         self.__btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
-
+        self.__process_window = tk.Text()
+        self.__process_window.pack(side="right")
+        self.__process_window.insert(tk.END, "Initializing Camera {}...".format(camera_num))
         self.__gui_root.wm_title("Test")
+
         self.__panel = None
         self.__destroyed = False
         self.__isrunning = True
         self.__event_level = 1
+        self.__camera_num = camera_num
+        self.__test_video = cv2.VideoCapture(camera_num)
 
-        self.__test_video = cv2.VideoCapture(0)
-        
-
-        self.__detector = Detector(method="Haar_frontalface", video_handler=self.__test_video)
-        self.__executor = threading.Thread(target=self.__get_frame)
         self.__image = None
-        self.__executor.isdaemon = True
-        self.__executor.start()
+        self.__event_str = None
+        self.__event_changed = False
+
+        self.__detector = Detector(method="Haar_upperbody", video_handler=self.__test_video)
+
+        self.__frame_executor = threading.Thread(target=self.__get_frame)
+        self.__frame_executor.setDaemon(True)
+        self.__frame_executor.start()
+
+        self.__event_lvl_executor = threading.Thread(target=self.__get_event_level)
+        self.__event_lvl_executor.setDaemon(True)
+        self.__event_lvl_executor.start()
         # self.__executor.join()
 
     def __destroy(self):
-        print("deleting detctor")
         del self.__detector
-        print("release camera")
         self.__test_video.release()
-        print("set it to true")
         self.__destroyed = True
 
         self.__isrunning = False
-        print("destoy frame")
         self.__gui_root.destroy()
-        print("quit gui")
         self.__gui_root.quit()
-        print("Done")
-        # exit(1)
 
     def __get_frame(self):
 
@@ -70,14 +73,26 @@ class Interface(object):
     def __get_event_level(self):
         while(self.__isrunning):
             if not self.__destroyed:
-                event_lvl = self.__detector.get_event_level()
+                try:
+                    event_lvl = self.__detector.get_event_level()
+                except AttributeError:
+                    break
+
             if event_lvl != self.__event_level:
+                time_now = str(datetime.now())[:-7]
+                verb = "raised to" if event_lvl > self.__event_level else "lowered to"
+
+                fmt = "[{:}] Camera {} Event {} {} {}\n".format(\
+                time_now, self.__camera_num, self.__event_level, verb, event_lvl
+                )
+                self.__event_level = event_lvl
+                self.__event_str = fmt
+                self.__event_changed = True
 
     
     def run(self):
         while(self.__isrunning):
             if self.__image is not None:
-                # print("running GUI")
                 im = Image.fromarray(self.__image)
 
                 if not self.__destroyed:
@@ -92,6 +107,10 @@ class Interface(object):
                 else:
                     self.__panel.configure(image=im)
                     self.__panel.image = im
+                
+                if self.__event_changed:
+                    self.__process_window.insert(tk.END, self.__event_str)
+                    self.__event_changed = False
                 self.__gui_root.update()
 
     def __del__(self):
