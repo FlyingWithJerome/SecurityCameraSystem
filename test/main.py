5db3faaba8085220b2eda8be8ce5fdb41c88aa5a
FlyingWithJerome <jiarunm@uci.edu>
2018-04-25 14:33:24 -0400

    make the use of gevent

--- test/main.py
+++ test/main.py
@@ -8,6 +8,8 @@ import threading
 import multiprocessing
 import sys
 
+import gevent
+
 from GUI_Interface import GUIInterface
 from Text_Interface import TextInterface
 
@@ -22,14 +24,18 @@ def _gui_interface_wrapper(camera_serial, is_pi):
 def launch_camera_instances(camera_num=0, interface_opt="text", is_pi=False):
 
     job_query = []
-    for num in range(camera_num):
+    for num in range(0, camera_num, 1):
         if interface_opt == "text":
-            job_query.append(threading.Thread(target=_text_interface_wrapper, args=(num, is_pi)))
-        else:
-            job_query.append(threading.Thread(target=_gui_interface_wrapper, args=(num, is_pi)))
+            print("lauching", camera_num)
+            job_query.append(gevent.spawn(_text_interface_wrapper, num, is_pi))
 
-    for job in job_query:
-        job.start()
+    gevent.joinall(job_query)
+    #         job_query.append(threading.Thread(target=_text_interface_wrapper, args=(num, is_pi)))
+    #     else:
+    #         job_query.append(threading.Thread(target=_gui_interface_wrapper, args=(num, is_pi)))
+
+    # for job in job_query:
+    #     job.start()
 
     # for job in job_query:
     #     job.join()
@@ -38,4 +44,4 @@ if __name__ == "__main__":
     # _text_interface_wrapper(0)
     camera_num = int(sys.argv[1])
     is_pi = True if sys.argv[2] == "pi" else False
-    launch_camera_instances(camera_num, "text", is_pi)
+    launch_camera_instances(camera_num=camera_num, interface_opt="text", is_pi=is_pi)
